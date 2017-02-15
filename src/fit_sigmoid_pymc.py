ae0b770bee9584bf3f459ee347682e3ca242caad
mdekauwe <mdekauwe@gmail.com>
2017-02-16 09:44:18 +1100

    refactored sigmoid fit

--- src/fit_sigmoid_pymc.py
+++ src/fit_sigmoid_pymc.py
@@ -21,6 +21,24 @@ import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
 
+def fitMe(df):
+
+    with pm3.Model() as sig_model:
+        a = pm3.Normal('a', mu=0, sd=1e2)
+        b = pm3.Normal('b', mu=0, sd=1e2)
+        sigma = pm3.Uniform('sigma', lower=0, upper=1000)
+        model = tt_sigmoid(df.sw.values, a, b)
+        error = pm3.Normal('error', mu=model, sd=sigma,
+                           observed=df.beta.values)
+
+    with sig_model:
+        # find an optimal start position
+        start = pm3.find_MAP(model=sig_model.model, fmin=optimize.fmin_powell)
+        step = pm3.Metropolis()
+        mcmc_traces = pm3.sample(5e4, step=step, start=start, njobs=-1)
+
+    make_plot(mcmc_traces)
+
 def tt_sigmoid(x, a, b):
     """
     Sigmoid function using tensor
@@ -30,35 +48,7 @@ def tt_sigmoid(x, a, b):
 def np_sigmoid(x, a, b):
     return 1.0 / (1.0 + np.exp(-a * (x - b)))
 
-if __name__ == "__main__":
-
-    #x = np.array([0.0, 1.0, 3.0, 4.3, 7.0, 8.0, 8.5, 10.0, 12.0])
-    #y = np.array([0.01, 0.02, 0.04, 0.11, 0.43, 0.7, 0.89, 0.95, 0.99])
-    #df = pd.DataFrame({'sw':x, 'beta':y})
-
-    fname = "/Users/mdekauwe/Desktop/crap.csv"
-    df = pd.read_csv(fname)
-    df.columns = ["sw", "beta"]
-
-    # Have points where the soil is full but no or not much ET, screen these
-    screen = np.max(df.sw) * 0.9
-    #df.beta = np.where(df.sw>screen, np.max(df.beta), df.beta)
-    df.beta = np.where(df.sw>screen, 1.0, df.beta)
-
-    with pm3.Model() as sig_model:
-        a = pm3.Normal('a', mu=0, sd=1e2)
-        b = pm3.Normal('b', mu=0, sd=1e2)
-        sigma = pm3.Uniform('sigma', lower=0, upper=1000)
-        weight = df.sw.values**0.5/sigma
-        model = tt_sigmoid(df.sw.values, a, b)
-        error = pm3.Normal('error', mu=model, sd=weight,
-                           observed=df.beta.values)
-
-    with sig_model:
-        # find an optimal start position
-        start = pm3.find_MAP(model=sig_model.model, fmin=optimize.fmin_powell)
-        step = pm3.Metropolis()
-        mcmc_traces = pm3.sample(5e4, step=step, start=start, njobs=-1)
+def make_plot(mcmc_traces):
 
     pm3.traceplot(mcmc_traces)
 
@@ -67,7 +57,8 @@ if __name__ == "__main__":
     b_post = mcmc_traces["b"][:, None]
 
     # dimension to plot along
-    swx = np.linspace(400, 650, 1000)[:, None]
+    #swx = np.linspace(400, 650, 1000)[:, None]
+    swx = np.linspace(0, 16, 16)[:, None]
 
     # mean prediction
     beta_pred = np_sigmoid(swx.T, a_post, b_post)
@@ -86,3 +77,25 @@ if __name__ == "__main__":
     plt.ylabel("Beta")
     plt.legend(loc="upper left")
     plt.show()
+
+
+if __name__ == "__main__":
+
+    x = np.array([0.0, 1.0, 3.0, 4.3, 7.0, 8.0, 8.5, 10.0, 12.0])
+    y = np.array([0.01, 0.02, 0.04, 0.11, 0.43, 0.7, 0.89, 0.95, 0.99])
+    df = pd.DataFrame({'sw':x, 'beta':y})
+
+    fitMe(df)
+
+    """
+    fname = "/Users/mdekauwe/Desktop/crap.csv"
+    df = pd.read_csv(fname)
+    df.columns = ["sw", "beta"]
+
+    # Have points where the soil is full but no or not much ET, screen these
+    screen = np.max(df.sw) * 0.9
+    #df.beta = np.where(df.sw>screen, np.max(df.beta), df.beta)
+    df.beta = np.where(df.sw>screen, 1.0, df.beta)
+
+    swx = np.linspace(400, 650, 1000)[:, None]
+    """
