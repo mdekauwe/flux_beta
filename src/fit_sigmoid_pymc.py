399080510b3adc0e885bc72be04166c0688ec974
mdekauwe <mdekauwe@gmail.com>
2017-02-16 20:49:50 +1100

    Changed funcs back

--- src/fit_sigmoid_pymc.py
+++ src/fit_sigmoid_pymc.py
@@ -54,12 +54,10 @@ def tt_sigmoid(x, a, b):
     """
     Sigmoid function using tensor
     """
-    #return 1.0 / (1.0 + tt.exp(-a * (x - b)))
-    return 1.0 / (1.0 + tt.exp(-a * x + b))
+    return 1.0 / (1.0 + tt.exp(-a * (x - b)))
 
 def np_sigmoid(x, a, b):
-    #return 1.0 / (1.0 + np.exp(-a * (x - b)))
-    return 1.0 / (1.0 + np.exp(np.dot(-a, x) + b))
+    return 1.0 / (1.0 + np.exp(-a * (x - b)))
 
 def make_plot(df, site, mcmc_traces, x_range, to_screen):
 
@@ -86,6 +84,7 @@ def make_plot(df, site, mcmc_traces, x_range, to_screen):
     plt.ylabel("Beta")
     #plt.legend(loc="upper left")
     if to_screen:
+        pm.traceplot(mcmc_traces)
         plt.show()
     else:
         plt.savefig("plots/%s.png" % (site), dpi=80)
